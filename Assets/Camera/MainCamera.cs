f0d5eb7938626dcbbf0153efc7ca908f6ed8ec5c
anakazaw <133791498+anakazaw@users.noreply.github.com>
2023-09-11 00:55:57 +0900

    commit

--- Assets/Camera/MainCamera.cs
+++ Assets/Camera/MainCamera.cs
@@ -9,7 +9,8 @@ public class MainCamera : MonoBehaviour
 
 
     public float camera_length = 2f;//プレイヤーからのカメラの引き具合
-    float camera_ang;//プレイヤーからの角度
+    [SerializeField]float camera_ang = 270f;//プレイヤーからの角度
+    [SerializeField]float camera_height = 2.2f;//プレイヤーの頭上からの高さ
 
     [SerializeField]float cameraY_max = 1f;//CameraのY軸の最大値
     [SerializeField]float cameraY_min = -1f;//CameraのY軸の最小値
@@ -17,7 +18,11 @@ public class MainCamera : MonoBehaviour
     public float cameraX_speed_rate = 1f;//カメラX軸の速度割合
     public float cameraY_speed_rate = 1f;//カメラY軸の速度割合
 
+<<<<<<< Updated upstream
     [SerializeField,Tooltip("二乗曲線")]bool multiple_flag = true;
+=======
+    [SerializeField,Tooltip("なめらかに動かす")]bool smooth_flag = true;
+>>>>>>> Stashed changes
     [SerializeField, Tooltip("球状に移動する")] bool sphere_flag = false;
 
     // Start is called before the first frame update
@@ -26,13 +31,17 @@ public class MainCamera : MonoBehaviour
         if (CameraLook == null) Debug.Log("CameraLookがない");
 
         //初期化
+<<<<<<< Updated upstream
         camera_ang = 270;
         this.transform.localPosition = - Vector3.forward * camera_length;
+=======
+        this.transform.position = CameraLook.transform.position - Vector3.forward * camera_length;
+>>>>>>> Stashed changes
 
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         //マウスのx軸、y軸取得
         float Mouse_X = Input.GetAxis("Mouse X");
@@ -47,12 +56,22 @@ public class MainCamera : MonoBehaviour
 
     void CameraMove(float mx ,float my)
     {
+<<<<<<< Updated upstream
         Vector3 Camera_Pos = this.transform.localPosition;
 
         //Camera横移動
         if (multiple_flag)
         {
             //二乗曲線
+=======
+        Vector3 CameraLook_Pos = CameraLook.transform.position;
+        Vector3 Camera_Pos = Vector3.zero;
+
+        //Camera横移動
+        if (smooth_flag)
+        {
+            //smooth (smooth damp)
+>>>>>>> Stashed changes
             camera_ang -= cameraX_speed_rate * multiply(mx);
         }
         else
@@ -63,25 +82,41 @@ public class MainCamera : MonoBehaviour
         
 
         //Cameraの上下移動
+<<<<<<< Updated upstream
         if (multiple_flag)
         {
             //二乗曲線
             Camera_Pos.y -= cameraY_speed_rate * multiply(my);
+=======
+        if (smooth_flag)
+        {
+            //smooth (smooth damp)
+            camera_height -= cameraY_speed_rate * multiply(my);
+>>>>>>> Stashed changes
         }
         else
         {
             //リニア
+<<<<<<< Updated upstream
             Camera_Pos.y -= my * cameraY_speed_rate;
         }
 
 
         Camera_Pos.y = Mathf.Clamp(Camera_Pos.y, cameraY_min, cameraY_max);
+=======
+            camera_height -= my * cameraY_speed_rate;
+        }
+
+        camera_height = Mathf.Clamp(camera_height, cameraY_min, cameraY_max);//高さを制限
+
+>>>>>>> Stashed changes
 
         float LookAt_to_Camera = camera_length;
 
         if (sphere_flag)
         {
             //球状に移動
+<<<<<<< Updated upstream
             LookAt_to_Camera = CameraLook.transform.localPosition.y - Camera_Pos.y;
             LookAt_to_Camera  = Mathf.Cos(Mathf.Asin(LookAt_to_Camera / camera_length)) * camera_length;
         
@@ -100,4 +135,26 @@ public class MainCamera : MonoBehaviour
         return x*Mathf.Abs(x);
     }
 
+=======
+            LookAt_to_Camera = camera_height;
+            LookAt_to_Camera  = Mathf.Cos(Mathf.Asin(LookAt_to_Camera / camera_length)) * camera_length;
+        
+        }
+
+        Camera_Pos.x = Mathf.Cos(camera_ang);
+        Camera_Pos.z = Mathf.Sin(camera_ang);
+        Camera_Pos *= LookAt_to_Camera;
+
+        Camera_Pos.y = camera_height;
+
+        this.transform.position = Camera_Pos + CameraLook_Pos;
+       
+    }
+
+    float multiply(float x)
+    {
+        return x*Mathf.Abs(x);
+    }
+
+>>>>>>> Stashed changes
 }
